9e20738310f7d611cb83394d685f465fcbdfe824
GDQE <technical.support.project1@gmail.com>
2026-06-24 13:02:25 +0300

    Delete rga-GDQE/src/pages/committee/committee/ProtectedRoute.jsx

--- rga-GDQE/src/pages/committee/committee/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { getCommitteeSession } from '../../services/committeeAuthService';
-
-export function CommitteeProtectedRoute({ children }) {
-  const session = getCommitteeSession();
-  if (!session) return <Navigate to="/committee/login" replace />;
-  return children;
-}
-
